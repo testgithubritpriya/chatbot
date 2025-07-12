ee833d65c499d71e9bf32795183de3de3ba1bb50
priyanka jootooru <137708100+testgithubritpriya@users.noreply.github.com>
2025-07-12 22:53:53 +0530

    update react1234
    Signed-off-by: priyanka jootooru <137708100+testgithubritpriya@users.noreply.github.com>

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React123
+          Learn React12345
         </a>
       </header>
     </div>
